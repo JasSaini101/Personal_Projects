d64f3c613d227f12ac9a8aed8f7ff7ab1f812489
Jasnoor Saini <83828348+JasSaini101@users.noreply.github.com>
2024-01-07 16:01:16 -0500

    Delete Personal_Projects directory

--- Personal_Projects/Snake Game (Java)/src/main/java/org/example/SnakeGame.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class SnakeGame {
-    public static void main(String[] arg){
-
-        //Same as GameFrame x = new GameFrame, but we need an x
-        new GameFrame();
-    }
-}
